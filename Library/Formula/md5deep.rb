a8fa8999151e8f011e8f0fac79ae95a109ed694a
Jonathan Wilkins <jwilkins@bitland.net>
2013-03-01 11:37:46 -0800

    added head option to md5deep/hashdeep formula

--- Library/Formula/md5deep.rb
+++ Library/Formula/md5deep.rb
@@ -5,7 +5,15 @@ class Md5deep < Formula
   url 'http://sourceforge.net/projects/md5deep/files/md5deep/md5deep-4.3/md5deep-4.3.tar.gz'
   sha1 'b9dd6444f07c9fc344ebef201baebdf71bda337f'
 
+  head 'git://github.com/jessek/hashdeep'
+
+  if build.head?
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
   def install
+    system "sh bootstrap.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make install"
   end
